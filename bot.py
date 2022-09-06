934e4f60a1480ad7ec40773a577610688132672f
Kasztandor <michal.swoboda@interia.pl>
2022-09-06 21:59:46 +0200

    auto MEE6 giving roles

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
-roles = [1016433847296086037,1016433864303980545,1016433874043162655,1016433882507255829,1016433889864069211,1016433896654639114,1016433907220107274,1016433916862799893,1016433924211232899,1016433930418798655,1016433939096817836]
-#[698047161245630545,698047575013457931,703327203068346388,723329755465777193,732345037798506497,736570299537031280,736570295954964542,736570323234848930,736570319921348619,736571825043013672,736571823600435280]
+# to testbot
+#roles = [1016433847296086037,1016433864303980545,1016433874043162655,1016433882507255829,1016433889864069211,1016433896654639114,1016433907220107274,1016433916862799893,1016433924211232899,1016433930418798655,1016433939096817836]
+# to ratcraft
+roles = [698047161245630545,698047575013457931,703327203068346388,723329755465777193,732345037798506497,736570299537031280,736570295954964542,736570323234848930,736570319921348619,736571825043013672,736571823600435280]
 
 import discord
+from discord.utils import get
 from math import floor
 
 class MyClient(discord.Client):
@@ -18,27 +21,25 @@ client = MyClient(intents=intents)
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.author.id == 386237687008591895:
+    if message.author.id == 159985870458322944: # Kasztandor: 386237687008591895 MEE6: 159985870458322944
+        guild = message.guild
         msg = message.content
         userID = msg[(msg.find("<@")+2):msg.find(">")]
+        #theUser = get(guild.members, id=userID)
+        theUser = message.mentions[0]
         level = int(msg[(msg.find("level")+6):msg.find("!")])
-        if level > 100:
-            fixedLevel = 100
-        else:
-            fixedLevel = level
-        roleToGive = roles[floor(fixedLevel/10)]
-        if fixedLevel < 10:
-            roleToRevoke = None
-        else:
-            roleToRevoke = roles[floor(fixedLevel/10)-1]
-        await message.channel.send(userID+" "+str(fixedLevel)+": +<@&"+str(roleToGive)+"> -<@&"+str(roleToRevoke)+">")
-        print(message.content)
-
-    elif message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        rankPosition = floor(level/10)
+        if rankPosition > 100:
+            rankPosition = 10
+        roleToGiveID = roles[rankPosition]
+        roleToGive = discord.utils.get(guild.roles,id=roleToGiveID)
+        await theUser.add_roles(roleToGive)
+        if rankPosition > 0:
+            roleToRevokeID = roles[rankPosition-1]
+            roleToRevoke = discord.utils.get(guild.roles,id=roleToRevokeID)
+            await theUser.remove_roles(roleToRevoke)
+        if level/10 == floor(level/10):
+            await message.channel.send("Gratulacje <@"+str(userID)+"> Właśnie osiągnąłeś rangę <@&"+str(roleToGiveID)+">")
 
 
 client.run('NzIyNzQ0NzI1NzU2NzA2ODU3.GXlyrf.yGGwtT5EvsvbmrSgUhDf0VPcUUv5rYsQRSmzpE')
